5f172d41ce00f409cca540ed5e5bcfb1cf489367
nuchaanrdy <mushroom_nuch@gmail.com>
2020-07-19 22:58:20 +0700

    update file

--- world.py
+++ world.py
@@ -1,1 +1,8 @@
-print ("WORLD!!!")
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
